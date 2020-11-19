f627b3f3e80a03e475ed931d90920043a9bcff0b
Shadax-stack <58188800+Shadax-stack@users.noreply.github.com>
2020-11-18 20:12:55 -0800

    Changed KRYPTON_API to be fully in the header file

--- src/Krypton/Krypton.h
+++ src/Krypton/Krypton.h
@@ -1,6 +1,26 @@
 #ifndef KRYPTON_H
 #define KRYPTON_H
 
+#ifdef KRYPTON_SHARED
+#ifdef _WIN32
+#define KRYPTON_WIN32
+#ifdef _MSC_VER 
+#define KRYPTON_EXPORT _declspec(dllexport)
+#define KRYPTON_IMPORT _declspec(dllimport)
+#endif
+#ifdef __GNUC__
+#define KRYPTON_EXPORT _declspec(dllexport)
+#define KRYPTON_IMPORT _declspec(dllimport)
+#endif
+#else
+#define KRYPTON_EXPORT __attribute__((visibility(default))
+#define KRYPTON_IMPORT
+#endif
+#else
+#define KRYPTON_EXPORT
+#define KRYPTON_IMPORT
+#endif
+
 #ifdef KRYPTON_BUILD
 #define KRYPTON_API KRYPTON_EXPORT
 #else
